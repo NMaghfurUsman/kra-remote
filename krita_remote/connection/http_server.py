a7ad2aac898b4d4a9b8a9aa072a58829d412c3df
Nabil Maghfur Usman <nmaghfurusman@gmail.com>
2024-04-16 14:47:54 +0800

    python: Serve the client from the extension folder

--- krita_remote/connection/http_server.py
+++ krita_remote/connection/http_server.py
@@ -2,10 +2,13 @@ from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
 import threading
 from socket import gethostname, gethostbyname
 from random import randint
+from ..api_krita import Krita
+
+INDEX_DIR = Krita.instance.getAppDataLocation() + "/pykrita/krita_remote/client"
 
 class Handler(SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, directory="./client", **kwargs)
+        super().__init__(*args, directory=INDEX_DIR, **kwargs)
 
 class HTTPServer(ThreadingHTTPServer):
     address: str
